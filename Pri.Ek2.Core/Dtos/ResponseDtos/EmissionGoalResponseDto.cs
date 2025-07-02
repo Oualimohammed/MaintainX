70f725d2c60ba8f5b60c82ac6fe1b9922a9bef4a
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-02 12:59:02 +0200

    Add EmissionGoal DTOs (Request/Response)

---
+++ Pri.Ek2.Core/Dtos/ResponseDtos/EmissionGoalResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Dtos.ResponseDtos
+{
+    public class EmissionGoalResponseDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public decimal TargetEmissionsKg { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal CurrentEmissionsKg { get; set; }
+        public bool IsAchieved { get; set; }
+    }
+}
