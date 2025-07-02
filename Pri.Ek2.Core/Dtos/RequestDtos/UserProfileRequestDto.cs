c391bf5462fa3014c988a4be67a9bdb493db6ad4
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-02 13:00:50 +0200

    Add UserProfile DTOs (Request/Response)

---
+++ Pri.Ek2.Core/Dtos/RequestDtos/UserProfileRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Dtos.RequestDtos
+{
+    public class UserProfileRequestDto
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthData { get; set; }
+
+        public string? ProfileImagePath { get; set; }
+    }
+}
