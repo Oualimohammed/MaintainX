11630b352ef454db866fc659d9e9828f10d403b1
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-02 12:57:27 +0200

    Add MaitenanceLog DTOs (Request/Response)

---
+++ Pri.Ek2.Core/Dtos/RequestDtos/MaintenanceLogRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Dtos.RequestDtos
+{
+    public class MaintenanceLogRequestDto
+    {
+        [Required]
+        public int VehicleId { get; set; }
+
+        [Required]
+        public DateTime MaintenanceDate { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal? NewEmissionFactor { get; set; }
+    }
+}
