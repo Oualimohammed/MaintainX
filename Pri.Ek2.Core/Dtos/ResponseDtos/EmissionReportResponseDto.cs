30184a4b19442a677a37162be16eaf5bae6a8fb1
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-02 12:58:42 +0200

    Add EmissionReport DTOs (Request/Response)

---
+++ Pri.Ek2.Core/Dtos/ResponseDtos/EmissionReportResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Dtos.ResponseDtos
+{
+    public class EmissionReportResponseDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public decimal TotalEmissionsKg { get; set; }
+        public DateTime PeriodStart { get; set; }
+        public DateTime PeriodEnd { get; set; }
+        public string? Notes { get; set; }
+    }
+}
