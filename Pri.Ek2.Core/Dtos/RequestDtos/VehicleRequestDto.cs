b0dea8713f489cc15b73b2528efcbb410fb662a8
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-02 12:58:19 +0200

    Add Vehicle DTOs (Request/Response)

---
+++ Pri.Ek2.Core/Dtos/RequestDtos/VehicleRequestDto.cs
@@ -0,0 +1,25 @@
+﻿using Pri.Ek2.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Dtos.RequestDtos
+{
+    public class VehicleRequestDto
+    {
+        [Required]
+        public string LicensePlate { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public VehicleType Type { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal EmissionFactor { get; set; }
+    }
+}
