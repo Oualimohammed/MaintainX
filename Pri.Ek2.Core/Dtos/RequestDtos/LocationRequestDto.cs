4cfaf79fea0c8d102ac0b6727701857014deecb4
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-03 10:38:33 +0200

    Renamed LocationsRequestDto to LocationRequestDto

---
+++ Pri.Ek2.Core/Dtos/RequestDtos/LocationRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Dtos.RequestDtos
+{
+    public class LocationRequestDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Range(-90,90)]
+        public double Latitude { get; set; }
+
+        [Range(-180,180)]
+        public double Longitude { get; set; }
+    }
+}
