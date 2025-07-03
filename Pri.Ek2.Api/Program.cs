9ca9926360d7a995468e339aecd79d3673664ef3
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-03 13:53:14 +0200

    Add jwt and swager to Program class and Update the appsettings.json ffile , -Add New Migrations and update the database.

--- Pri.Ek2.Api/Program.cs
+++ Pri.Ek2.Api/Program.cs
@@ -1,9 +1,13 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Pri.Ek2.Core.Data;
 using Pri.Ek2.Core.Services.Implementations;
 using Pri.Ek2.Core.Services.Interfaces;
+using System.Text;
 
 namespace Pri.Ek2.Api
 {
@@ -20,6 +24,17 @@ namespace Pri.Ek2.Api
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+
+            //jwt token configuration
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => options.TokenValidationParameters = new()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
+
+
             // Add services to the container.
             builder.Services.AddScoped<IVehicleService, VehicleService>();
             builder.Services.AddScoped<IEmissionReportService, EmissionReportService>();
@@ -29,12 +44,23 @@ namespace Pri.Ek2.Api
             builder.Services.AddScoped<IRewardService, RewardService>();
             builder.Services.AddScoped<IMaintenanceLogService, MaintenanceLogService>();
             builder.Services.AddScoped<ILocationService, LocationService>();
-
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<ITokenService, TokenService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // swagger configuration
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+            });
 
             var app = builder.Build();
 
@@ -46,6 +72,8 @@ namespace Pri.Ek2.Api
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
--- Pri.Ek2.Core/Migrations/20250703113849_AddFieldEmailAndFirstAndLastNameToEntitie.Designer.cs
+++ Pri.Ek2.Core/Migrations/20250703113849_AddFieldEmailAndFirstAndLastNameToEntitie.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pri.Ek2.Core.Data;
 
@@ -11,9 +12,11 @@ using Pri.Ek2.Core.Data;
 namespace Pri.Ek2.Core.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250703113849_AddFieldEmailAndFirstAndLastNameToEntitie")]
+    partial class AddFieldEmailAndFirstAndLastNameToEntitie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
