af00340234998e625ca5729a47af0c2f0f8f51e9
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-05 11:47:50 +0200

    Update ITokenService And his Implementation TokenService

--- Pri.Ek2.Core/Services/Implementations/TokenService.cs
+++ Pri.Ek2.Core/Services/Implementations/TokenService.cs
@@ -5,16 +5,13 @@ using Pri.Ek2.Core.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Pri.Ek2.Core.Services.Implementations
 {
     public class TokenService : ITokenService
     {
-
         private readonly IConfiguration _configuration;
 
         public TokenService(IConfiguration configuration)
@@ -22,30 +19,40 @@ namespace Pri.Ek2.Core.Services.Implementations
             _configuration = configuration;
         }
 
-        public string GenerateToken(IdentityUser identityUser)
+        public string GenerateToken(IdentityUser identityUser, IList<string> roles)
         {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, identityUser.Id),
+                new Claim(JwtRegisteredClaimNames.UniqueName, identityUser.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, identityUser.Email ?? string.Empty),
+                // Standaard claim types voor compatibiliteit
+                new Claim(ClaimTypes.NameIdentifier, identityUser.Id),
+                new Claim(ClaimTypes.Name, identityUser.UserName),
+                new Claim(ClaimTypes.Email, identityUser.Email ?? string.Empty)
+            };
+
+            // Voeg rollen toe met beide claim types
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+                claims.Add(new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", role));
+            }
+
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiryInHours = int.Parse(_configuration["Jwt:ExpiryInHours"] ?? "4");
 
             var token = new JwtSecurityToken(
                 issuer: _configuration["Jwt:Issuer"],
                 audience: _configuration["Jwt:Audience"],
-                claims: null,
-                expires: DateTime.Now.AddMinutes(30),
+                claims: claims,
+                expires: DateTime.Now.AddHours(expiryInHours),
                 signingCredentials: creds
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
-
-        private IEnumerable<Claim> GetClaims(IdentityUser identityUser)
-        {
-            return new List<Claim>
-            {
-                new Claim(ClaimTypes.NameIdentifier, identityUser.Id),
-                new Claim(ClaimTypes.Name, identityUser.UserName),
-                new Claim(ClaimTypes.Email, identityUser.Email ?? string.Empty)
-            };
-    }
     }
 }
--- Pri.Ek2.Core/Services/Interfaces/ITokenService.cs
+++ Pri.Ek2.Core/Services/Interfaces/ITokenService.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pri.Ek2.Core.Services.Interfaces
 {
     public interface ITokenService
     {
-        string GenerateToken(IdentityUser identityUser);
+        string GenerateToken(IdentityUser identityUser, IList<string> roles);
     }
 }
