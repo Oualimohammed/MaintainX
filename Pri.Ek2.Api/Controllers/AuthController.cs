db160c1f3248cc0bb44c054da3be67c84c515f2b
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-05 11:57:27 +0200

    feat(auth): add AuthController
    - Add login/register endpoints

---
+++ Pri.Ek2.Api/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.AuthDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+
+namespace Pri.Ek2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var response = await _authService.LoginAsync(loginRequestDto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var response = await _authService.RegisterAsync(registerRequestDto);
+                return CreatedAtAction(nameof(Login), new { email = registerRequestDto.Email }, response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
