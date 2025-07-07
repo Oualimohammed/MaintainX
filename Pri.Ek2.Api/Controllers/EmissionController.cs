4c3974d5f7ce599a6def927f092f0e6960287644
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-07 13:39:51 +0200

    Add EmissionController for handling emission operations

---
+++ Pri.Ek2.Api/Controllers/EmissionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+using System.Security.Claims;
+
+namespace Pri.Ek2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmissionController : ControllerBase
+    {
+        private readonly IEmissionGoalService _goalService;
+        private readonly IEmissionReportService _reportService;
+
+        public EmissionController(
+            IEmissionGoalService goalService,
+            IEmissionReportService reportService)
+        {
+            _goalService = goalService;
+            _reportService = reportService;
+        }
+
+       
+    }
+}
