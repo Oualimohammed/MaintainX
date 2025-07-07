0330cb66cb114abc74c6eb0081b9a6323f40f715
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-07 14:22:58 +0200

    Add RewardsController with authorization and services

---
+++ Pri.Ek2.Api/Controllers/RewardsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+
+namespace Pri.Ek2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RewardsController : ControllerBase
+    {
+        private readonly IRewardService _rewardService;
+
+        public RewardsController(IRewardService rewardService)
+        {
+            _rewardService = rewardService;
+        }
+
+    }
+}
