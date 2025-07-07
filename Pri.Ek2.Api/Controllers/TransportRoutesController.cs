3962d88281f754e484e260c97bf6fc0824351018
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-07 13:28:36 +0200

    Add TransportRoutesController for handling transport routes.

---
+++ Pri.Ek2.Api/Controllers/TransportRoutesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+
+namespace Pri.Ek2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TransportRoutesController : ControllerBase
+    {
+        private readonly ITransportRouteService _routeService;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public TransportRoutesController(
+            ITransportRouteService routeService,
+            IWebHostEnvironment hostEnvironment)
+        {
+            _routeService = routeService;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        
+
+    }
+}
