443f86fd952743de21fb66ccfd1dd9fc0334e3a1
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-07 12:18:18 +0200

    Add GetAll eindpoint to UserProfileController

---
+++ Pri.Ek2.Api/Controllers/UserProfileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+using System.Security.Claims;
+
+namespace Pri.Ek2.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileService _profileService;
+
+        public UserProfileController(IUserProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<IEnumerable<UserProfileResponseDto>>> GetAll()
+        {
+            var profiles = await _profileService.GetAllUserProfilesAsync();
+            return Ok(profiles);
+        }
+
+       
+    }
+}
