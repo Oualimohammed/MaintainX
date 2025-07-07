dee406db3e7acc8480d55b7e1a0603f4f6cfb6fd
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-07 14:16:59 +0200

    Add LocationsController with routing and authorization

---
+++ Pri.Ek2.Api/Controllers/LocationsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+
+namespace Pri.Ek2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ILocationService _locationService;
+
+        public LocationsController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+
+      
+    }
+}
