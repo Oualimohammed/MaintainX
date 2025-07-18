fd15e3ccec115b356a6f1c9a9850f7e72f5b5ce8
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-18 12:31:47 +0200

    Add UserProfileResponseDto class for user data handling.

---
+++ Pri.Ek2.Client/Dtos/ResponseDtos/UserProfileResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Pri.Ek2.Client.Dtos.ResponseDtos
+{
+    public class UserProfileResponseDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthData { get; set; }
+        public string? ProfileImagePath { get; set; }
+    }
+}
