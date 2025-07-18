93f44fef5e4e1b46c21dc8965bae57bcd2461bc7
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-18 12:28:23 +0200

    Add IAuthService interface with registration and login methods.

---
+++ Pri.Ek2.Client/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Pri.Ek2.Client.Dtos.AuthDtos;
+
+namespace Pri.Ek2.Client.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterRequestDto registerRequestDto);
+        Task<AuthResponseDto> LoginAsync(LoginRequestDto loginRequestDto);
+    }
+}
