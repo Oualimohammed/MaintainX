a4a3b84f61840080f5aa8d8fe2ce7051506663f1
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-18 12:25:56 +0200

    Add AuthResponseDto class for authentication responses.

---
+++ Pri.Ek2.Client/Dtos/AuthDtos/AuthResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using Pri.Ek2.Client.Dtos.ResponseDtos;
+
+namespace Pri.Ek2.Client.Dtos.AuthDtos
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public UserProfileResponseDto UserProfile { get; set; }
+    }
+}
