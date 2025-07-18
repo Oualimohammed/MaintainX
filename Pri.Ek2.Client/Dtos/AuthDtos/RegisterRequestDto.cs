cf6c2847bf5743e74f7faee091579c5df9d1221c
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-07-18 12:26:24 +0200

    Add DTO classes for login and registration.

---
+++ Pri.Ek2.Client/Dtos/AuthDtos/RegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Pri.Ek2.Client.Dtos.AuthDtos
+{
+    public class RegisterRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
