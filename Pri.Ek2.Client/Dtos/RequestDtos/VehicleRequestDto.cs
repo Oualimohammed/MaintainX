782115693340b957b39dcda8ef72f91b1f1ab3d9
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-08 01:36:31 +0200

    Add VehicleRequestDto and VehicleResponseDto classes

---
+++ Pri.Ek2.Client/Dtos/RequestDtos/VehicleRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using Pri.Ek2.Client.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Client.Dtos.RequestDtos
+{
+    public class VehicleRequestDto
+    {
+        public string LicensePlate { get; set; }
+        public string Model { get; set; }
+        public VehicleType Type { get; set; }
+        public decimal EmissionFactor { get; set; }
+    }
+}
---
+++ Pri.Ek2.Client/Dtos/ResponseDtos/VehicleResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using Pri.Ek2.Client.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Client.Dtos.ResponseDtos
+{
+    public class VehicleResponseDto
+    {
+        public int Id { get; set; }
+        public string LicensePlate { get; set; }
+        public string Model { get; set; }
+        public decimal EmissionFactor { get; set; }
+        public VehicleType Type { get; set; }
+        public int CurrentMileage { get; set; }
+        public string Status { get; set; }
+    }
+}
+
+
+
