6b6c16a078c784aa8176f7afae7f902be2d3ee78
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-10 16:39:01 +0200

    Add MaintenanceLogService and IMaintenanceLogService,
    Implemented the MaintenanceLogService class to manage maintenance logs, including methods for retrieving, adding, updating, deleting logs, and uploading attachments.

---
+++ Pri.Ek2.Client/Services/Interfaces/IMaintenanceLogService.cs
@@ -0,0 +1,16 @@
+﻿using Pri.Ek2.Client.Dtos.RequestDtos;
+using Pri.Ek2.Client.Dtos.ResponseDtos;
+
+namespace Pri.Ek2.Client.Services.Interfaces
+{
+    public interface IMaintenanceLogService
+    {
+        Task<IEnumerable<MaintenanceLogResponseDto>> GetAllAsync();
+        Task<MaintenanceLogResponseDto> GetByIdAsync(int id);
+        Task<IEnumerable<MaintenanceLogResponseDto>> GetLogsByVehicleAsync(int vehicleId);
+        Task<MaintenanceLogResponseDto> AddAsync(MaintenanceLogRequestDto dto);
+        Task<MaintenanceLogResponseDto> UpdateAsync(int id, MaintenanceLogRequestDto dto);
+        Task DeleteAsync(int id);
+        Task<string> UploadAttachmentAsync(int logId, MultipartFormDataContent content);
+    }
+}
