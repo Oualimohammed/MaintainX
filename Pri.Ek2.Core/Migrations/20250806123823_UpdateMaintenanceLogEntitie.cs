2788d787663515478c2e3193bf29def5510e8870
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-10 16:42:43 +0200

    Update MaintenanceLogs schema for attachment paths.

---
+++ Pri.Ek2.Core/Migrations/20250806123823_UpdateMaintenanceLogEntitie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pri.Ek2.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMaintenanceLogEntitie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentPaths",
+                table: "MaintenanceLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachmentPaths",
+                table: "MaintenanceLogs");
+        }
+    }
+}
