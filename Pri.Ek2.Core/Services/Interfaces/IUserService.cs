7c09dd0299c93e9e553ce6d2baeafdcb939949e6
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 13:19:33 +0200

    Implement UserService and IUserService interface. Created UserService to manage user operations including retrieval and password reset.

---
+++ Pri.Ek2.Core/Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Core.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<UserDto>> GetAllUsersAsync();
+        Task<UserDto> GetUserByIdAsync(string userId);
+        Task ResetPasswordAsync(ResetPasswordRequestDto dto);
+    }
+}
