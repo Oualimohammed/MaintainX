ac842a583f5aafc798817c3a3b29270152f4ff77
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 16:34:01 +0200

    Change UserId type to string in UserProfileRequestDto

--- Pri.Ek2.Core/Dtos/RequestDtos/UserProfileRequestDto.cs
+++ Pri.Ek2.Core/Dtos/RequestDtos/UserProfileRequestDto.cs
@@ -10,7 +10,7 @@ namespace Pri.Ek2.Core.Dtos.RequestDtos
     public class UserProfileRequestDto
     {
         [Required]
-        public int UserId { get; set; }
+        public string UserId { get; set; }
 
         [Required]
         public string FirstName { get; set; }
