0d9b3f4512cdf1e898679ea4f2da1daa306a1f35
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 16:24:54 +0200

    Refactor database schema and update migrations.

--- Pri.Ek2.Core/Migrations/20250806105340_InitialCreateAfterRefactor.Designer.cs
+++ Pri.Ek2.Core/Migrations/20250806105340_InitialCreateAfterRefactor.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pri.Ek2.Core.Data;
 
@@ -11,9 +12,11 @@ using Pri.Ek2.Core.Data;
 namespace Pri.Ek2.Core.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250806105340_InitialCreateAfterRefactor")]
+    partial class InitialCreateAfterRefactor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -227,9 +230,6 @@ namespace Pri.Ek2.Core.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<string>("AttachmentPathsJson")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Description")
                         .IsRequired()
