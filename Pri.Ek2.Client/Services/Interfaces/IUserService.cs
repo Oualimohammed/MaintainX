67e348b6d59e6ab3b63fa905fb4a0a314df71baa
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 13:23:00 +0200

    Add IUserService interface with user management methods.

---
+++ Pri.Ek2.Client/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Pri.Ek2.Client.Dtos.RequestDtos;
+using Pri.Ek2.Client.Dtos.ResponseDtos;
+
+namespace Pri.Ek2.Client.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<UserDto>> GetAllUsersAsync();
+        Task ResetPasswordAsync(ResetPasswordRequestDto dto);
+    }
+}
+
