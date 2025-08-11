92fab655e11d7cfeb4be768707968cde5f652a50
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 16:30:17 +0200

    Enhance MaintenanceLogRequestDto with validation messages

--- Pri.Ek2.Core/Dtos/RequestDtos/MaintenanceLogRequestDto.cs
+++ Pri.Ek2.Core/Dtos/RequestDtos/MaintenanceLogRequestDto.cs
@@ -9,16 +9,25 @@ namespace Pri.Ek2.Core.Dtos.RequestDtos
 {
     public class MaintenanceLogRequestDto
     {
-        [Required]
+        [Required(ErrorMessage = "Voertuig ID is verplicht")]
         public int VehicleId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Onderhoudsdatum is verplicht")]
         public DateTime MaintenanceDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Beschrijving is verplicht")]
+        [MaxLength(255, ErrorMessage = "Beschrijving mag maximaal 255 tekens bevatten")]
         public string Description { get; set; }
 
-        [Range(0, double.MaxValue)]
+        [Range(0, double.MaxValue, ErrorMessage = "Emissiefactor moet positief zijn")]
         public decimal? NewEmissionFactor { get; set; }
+
+        public int? MileageAtMaintenance { get; set; }
+
+        public List<string>? AttachmentPaths { get; set; }
+
+        public string? Status { get; set; }
+
+        public bool IsScheduled { get; set; }
     }
 }
