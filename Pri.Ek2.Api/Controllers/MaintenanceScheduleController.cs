d24de8ec432406db7fbe721fce252d0db74d1744
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 16:47:31 +0200

    Add MaintenanceScheduleController for schedule management.

---
+++ Pri.Ek2.Api/Controllers/MaintenanceScheduleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pri.Ek2.Core.Dtos.RequestDtos;
+using Pri.Ek2.Core.Dtos.ResponseDtos;
+using Pri.Ek2.Core.Services.Interfaces;
+
+namespace Pri.Ek2.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Mechanic,Admin")]
+    public class MaintenanceScheduleController : ControllerBase
+    {
+        private readonly IMaintenanceScheduleService _maintenanceScheduleService;
+
+        public MaintenanceScheduleController(IMaintenanceScheduleService maintenanceScheduleService)
+        {
+            _maintenanceScheduleService = maintenanceScheduleService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<MaintenanceScheduleResponseDto>>> GetAll()
+        {
+            var schedules = await _maintenanceScheduleService.GetAllAsync();
+            return Ok(schedules);
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<MaintenanceScheduleResponseDto>> GetById(int id)
+        {
+            try
+            {
+                var schedule = await _maintenanceScheduleService.GetByIdAsync(id);
+                return Ok(schedule);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound($"Onderhoudsschema {id} niet gevonden.");
+            }
+        }
+
+        
+    }
+}
+
