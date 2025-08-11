a61c3d840f03f16fbfc90c5869a6696054865cbe
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 21:27:36 +0200

    Add PriorityTask class to manage task attributes.

---
+++ Pri.Ek2.Client/Models/PriorityTask.cs
@@ -0,0 +1,12 @@
+﻿namespace Pri.Ek2.Client.Models
+{
+    public class PriorityTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; }
+        public string Link { get; set; }
+    }
+}
