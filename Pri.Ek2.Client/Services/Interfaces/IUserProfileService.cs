18790ad941cbac3f55aef74688f97dec037dd7ee
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-12 01:02:10 +0200

    Add IUserProfileService interface for user management.

---
+++ Pri.Ek2.Client/Services/Interfaces/IUserProfileService.cs
@@ -0,0 +1,14 @@
+﻿using Pri.Ek2.Client.Dtos.RequestDtos;
+using Pri.Ek2.Client.Dtos.ResponseDtos;
+
+namespace Pri.Ek2.Client.Services.Interfaces
+{
+    public interface IUserProfileService
+    {
+        Task<IEnumerable<UserProfileResponseDto>> GetAllUserProfilesAsync();
+        Task<UserProfileResponseDto> GetUserProfileAsync(string userId);
+        Task<UserProfileResponseDto> CreateUserProfileAsync(string userId, UserProfileRequestDto dto);
+        Task<UserProfileResponseDto> UpdateUserProfileAsync(string userId, UserProfileRequestDto dto);
+        Task DeleteUserProfileAsync(string userId);
+    }
+}
