db635461536b182b9c9e1f956793e183f10ed497
OUALI MohammedMMM <mohammed.ouali@student.howest.be>
2025-08-11 16:58:00 +0200

    Add MaintenanceLogRequestDto class for maintenance logs

---
+++ Pri.Ek2.Client/Dtos/RequestDtos/MaintenanceLogRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ek2.Client.Dtos.RequestDtos
+{
+    public class MaintenanceLogRequestDto
+    {
+        [Required]
+        public int VehicleId { get; set; }
+
+        [Required]
+        public DateTime MaintenanceDate { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal? NewEmissionFactor { get; set; }
+        public string Status { get; set; }
+        public bool IsScheduled { get; set; }
+        public int? MileageAtMaintenance { get; set; }
+
+        public List<string>? AttachmentPaths { get; set; }
+    }
+}
